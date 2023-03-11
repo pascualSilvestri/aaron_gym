6cf663b4c85f4e9168d7cbd0f7586fe5a0792e8b
pascual_silvestri <pascualsilvestri14@gmail.com>
2023-03-11 18:57:16 -0300

    estructura del proyecto completa

--- aaron_gym/aaron_gym/settings/base.py
+++ aaron_gym/aaron_gym/settings/base.py
@@ -23,8 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-jx#cx)qxy8@+jxpo!b+=3gk=i0d(g=oomtoh(&(*$yfyg_t_tj'
 
+AUTH_USER_MODEL = 'usuarios.Usuario'
 
 
+LANGUAGE_CODE = 'es-ar'
+
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 # Application definition
 
@@ -35,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.usuarios',
+    
 ]
 
 MIDDLEWARE = [
@@ -92,11 +98,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
@@ -104,7 +109,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS=(
+    os.path.join(os.path.dirname(BASE_DIR),"static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
