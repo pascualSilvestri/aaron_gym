942f96ab89baf0d2f6600af829edd871afdf4f55
pascual_silvestri <pascualsilvestri14@gmail.com>
2023-03-19 13:21:12 -0300

    modificaciones

---
+++ aaron_gym/static/js/menu.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.nav_respons');
+const btnMenu = document.querySelector('.menu');
+
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.toggle("mostrar")
+});
